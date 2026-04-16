c13c606368f09a17da729169c657dbbec7eaa382
jae9380 <ljy5314@gmail.com>
2026-04-16 17:50:27 +0900

    Fintracking Auth Server

---
+++ src/main/java/com/ft/auth/FintrackingAuthApplication.java
@@ -0,0 +1,13 @@
+package com.ft.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FintrackingAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FintrackingAuthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ft/auth/FintrackingAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ft.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintrackingAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
