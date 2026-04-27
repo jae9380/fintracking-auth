3abb33f559fe67de8948b6f6c68e6727a2e79269
jae9380 <ljy5314@gmail.com>
2026-04-27 20:40:26 +0900

    feat: Cloud Bus 연동 및 JwtTokenProvider @RefreshScope 적용

--- src/main/java/com/ft/auth/infrastructure/jwt/JwtTokenProvider.java
+++ src/main/java/com/ft/auth/infrastructure/jwt/JwtTokenProvider.java
@@ -6,6 +6,7 @@ import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
@@ -13,6 +14,7 @@ import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
 import java.util.Date;
 
+@RefreshScope
 @Component
 public class JwtTokenProvider implements TokenProvider {
 
